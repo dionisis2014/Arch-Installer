65109e6ca20b5dd29a74a2583064139e56b279c4
Unknown <unknown@example.com>
2019-12-07 02:35:44 +0200

    Rewrite

--- arch_installer.sh
+++ arch_installer.sh
@@ -1,180 +1,220 @@
 #!/bin/bash
 
-#Arch Linux automated install script by dionisis2014
+#   Arch Linux install script created by dionisis2014
+#
+#   User takes full responsibility for any damage
+#   done to the system by the usage of this script
 
-C_RESET="\033[0m"
-C_RED="\033[31m"
-C_GREEN="\033[32m"
-C_BLUE="\033[34m"
-C_CYAN="\033[36m"
-C_YELLOW="\033[33m"
+RST="\033[0;0m"
+BOLD="\033[1m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
 
-M_RESET="\033[0m"
-M_BOLD="\033[1m"
+EFI_SET=1
+HOSTNAME=""
+USERNAME=""
 
-if [ $(whoami) != "root" ]
-then
-    printf "${C_RED}Script must be run as root! Aborted installation!${C_RESET}\n"
-    exit 1
-fi
-
-printf "${M_BOLD}${C_CYAN}Arch Linux${C_RESET}${M_BOLD} automated install by ${C_GREEN}dionisis2014${C_RESET}\n\n"
-printf "${C_YELLOW}Start installation? [Y/N]: ${C_RESET}"
-read -r response
-if [[ ! $response =~ ^(([yY]|([yY][eE][sS])))$ && ! $response =~ ^$ ]]
-then
-    printf "${C_RED}Aborted installation!${C_RESET}\n"
-    exit 2
-fi
-echo
-
-#NETWORKING
-printf "${C_BLUE}${M_BOLD}Establishing DHCP connection ...${C_RESET}\n"
-dhcpcd
-echo
-printf "${C_BLUE}${M_BOLD}Testing connection ...${C_RESET}\n"
-if ping -q -c 5 -W 1 8.8.8.8 > /dev/null
-then
-    printf "${C_GREEN}Connection established${C_RESET}\n"
-else
-    printf "${C_RED}Connection failed! Aborted installation!${C_RESET}\n"
-    exit 3
-fi
-echo
-
-#TIME
-printf "${C_BLUE}${M_BOLD}Setting NTP ...${C_RESET}\n"
-timedatectl set-ntp true
-echo
-
-#PARTITIONING
-efi=false
-if [ -d "/sys/firmware/efi" ]
-then
-    printf "${C_BLUE}${M_BOLD}Running in EFI mode${C_RESET}\n"
-    efi=true
-fi
-printf "${C_BLUE}{M_BOLD}Getting all available disks ...${C_RESET}\n"
-disks=( $(lsblk -d -l -n | cut -d' ' -f1 | grep -E "[^loop^tmp^run]+") )
-printf "${M_BOLD}Found disks:${C_RESET}\n"
-printf "  %s\n" ${disks[@]}
-
-while true
-do
-    printf "${C_YELLOW}Select disk to format:${C_RESET} "
-    read -r disk
-    if [[ " ${disks[@]} " =~ " $disk " ]]
+function question () {
+    local answer
+    read -r answer
+    if [[ ${answer} =~ ^([yY]|([yY][eE][sS]))?$ ]]
     then
-        printf "${C_YELLOW}Selected disk ${C_RESET}\"$disk\"\n"
-        break
+        return 0
     else
-        printf "${C_RED}Selected disk ${C_RESET}\"$disk\"${C_RED} doesn't exist!${C_RESET}\n"
+        return 1
     fi
-done
+}
+
+function question_exit () {
+    question
+    if [[ $? == 0 ]]
+    then
+        return 0
+    else
+        printf "${BOLD}${RED}$1${RST}\n"
+        exit 1
+    fi
+}
+
+printf "${BOLD}${BLUE}Arch Linux${RST}${BOLD} installation script by${GREEN} dionisis2014${RST}\n"
+echo
+
+printf "${BOLD}${MAGENTA}Step 1: Answer questions about installation below${RST}\n"
+printf "${BOLD}${YELLOW}Are you ready to procced? [Y/N]: ${RST}"
+question_exit "Aborted installation!"
 
 while true
 do
-    printf "${C_BLUE}${M_BOLD}Opening cfdisk ...${C_RESET}\n"
-    cfdisk /dev/$disk
-    printf "${C_BLUE}${M_BOLD}Continue with installation? [Y/N]: ${C_RESET}"
+    printf "${BOLD}${YELLOW}Enter hostname: ${RST}"
     read -r response
-    if [[ $response =~ ^(([yY]|([yY][eE][sS])))$ || $response =~ ^$ ]]
+    if [[ ${response} =~ ^([a-zA-Z0-9.-_]+)$ ]]
     then
-        break
-    fi
-done
-
-printf "${C_BLUE}${M_BOLD}Opening new shell to format partitions. Type \"exit\" to continue installation.${C_RESET}\n"
-bash
-printf "${C_BLUE}${M_BOLD}Opening new shell to mount partitions to \"/mnt\"."
-if [ efi == true ]
-then
-    printf " Mount EFI partition to \"/mnt/efi\"."
-fi
-printf "Type exit to continue installation.${C_RESET}\n"
-bash
-
-#INSTALLATION
-printf "${C_BLUE}${M_BOLD}Updating pacman databases ...${C_RESET}\n"
-pacman -Sy
-printf "${C_BLUE}${M_BOLD}Installing archlinux-keyring ...${C_RESET}\n"
-pacman -S --noconfirm archlinux-keyring
-printf "${C_BLUE}${M_BOLD}Installing base system ...${C_RESET}\n"
-pacstrap /mnt linux linux-firmware nano reflector sudo git base-devel wget bash-completion openssh ntp bash networkmanager grub archlinux-keyring
-printf "${C_BLUE}${M_BOLD}Generating fstab ...${C_RESET}\n"
-genfstab -U /mnt >> /mnt/etc/fstab
-
-printf "${C_BLUE}${M_BOLD}Creating script file for arch-chroot environment ...${C_RESET}\n"
-printf "${C_BLUE}${M_BOLD}Entering arch-chroot ...${C_RESET}\n"
-arch-chroot /mnt << EOF
-
-#Arch Linux automated install script by dionisis2014
-
-C_RESET="\033[0m"
-C_RED="\033[31m"
-C_GREEN="\033[32m"
-C_BLUE="\033[34m"
-C_CYAN="\033[36m"
-C_YELLOW="\033[33m"
-
-M_RESET="\033[0m"
-M_BOLD="\033[1m"
-
-#CONFIGURATION
-printf "\${C_BLUE}\${M_BOLD}Setting time zone ...\${C_RESET}\n"
-ln -sf /usr/share/zoneinfo/Europe/Athens /etc/localtime
-printf "\${C_BLUE}\${M_BOLD}Setting clock RTC from system time ...\${C_RESET}\n"
-hwclock --systohc
-printf "\${C_BLUE}\${M_BOLD}Setting locale to \${C_YELLOW}en_US.UTF-8\${C_BLUE} ...\${C_RESET}\n"
-sed -i 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
-echo LANG=en_US.UTF-8 >> /etc/locale.conf
-printf "\${C_BLUE}\${M_BOLD}Generating locales ...\${C_RESET}\n"
-locale-gen
-printf "\${C_YELLOW}\${M_BOLD}Enter hostname: \${C_RESET}\n"
-read -r name
-echo "\$name" >> /etc/hostname
-printf "\${C_BLUE}\${M_BOLD}Generating hosts file ...\${C_RESET}\n"
-printf "\n127.0.0.1\tlocalhost\n::1\t\t\tlocalhost\n127.0.1.1\t\${name}.localdomain\t\${name}\n" >> /etc/hosts
-printf "\${C_YELLOW}\${M_BOLD}Setting root password ...\${C_RESET}\n"
-passwd
-printf "\${C_BLUE}\${M_BOLD}Installing CPU ucode ...\${C_RESET}\n"
-model=\$(cat /proc/cpuinfo | grep -m 1 "^vendor_id.*:" | cut -d' ' -f2)
-if [ "\$model" == "GenuineIntel" ]
-then
-    pacman -S --noconfirm intel-ucode
-else
-    pacman -S --noconfirm amd-ucode
-fi
-printf "\${C_BLUE}\${M_BOLD}Enabling services ...\${C_RESET}\n"
-systemctl enable sshd
-systemctl enable ntpd
-systemctl enable NetworkManager
-printf "\${C_BLUE}\${M_BOLD}Enabling wheel group ...\${C_RESET}\n"
-sed -i 's/^# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/locale.gen
-while true
-do
-    printf "\${C_YELLOW}\${M_BOLD}Enter new user name: \${C_RESET}\n"
-    read -r user
-    if [[ "\$user" =~ ^([A-Za-z0-9._-]+)\$ ]]
-    then
+        HOSTNAME=$response
         break
     else
-       printf "\${C_RED}Selected username \${C_RESET}\"\$user\"\${C_RED} is invalid!\${C_RESET}\n"
+        printf "${BOLD}${RED}Hostname ${RST}${BOLD}\"${response}\"${RED} is invalid!${RST}\n"
     fi
 done
-useradd -m -g users -G wheel -s /bin/bash "\$user"
-printf "\${C_YELLOW}\${M_BOLD}Setting user password ...\${C_RESET}\n"
-passwd "\$user"
-printf "\${C_BLUE}\${M_BOLD}Installing boot loader ...\${C_RESET}\n"
-if [ -d "/efi" ]
-then
-    pacman -S --noconfirm efibootmgr
-    grub-install --target=x86_64-efi --efi-directory=efi --bootloader-id=GRUB
-else
-    printf "\${C_BLUE}\${M_BOLD}Opening new shell to install bootloader. Type \"grub-install /dev/<disk>\" and then exit to continue installation.\${C_RESET}\n"
-    bash
-    grub-mkconfig -o /boot/grub/grub.cfg
-fi
-EOF
 
-printf "${C_GREEN}${M_BOLD}Installation finished! Shutdown computer and remove live media.\n"
+while true
+do
+    printf "${BOLD}${YELLOW}Enter username: ${RST}"
+    read -r response
+    if [[ ${response} =~ ^([a-zA-Z0-9.-_]+)$ ]]
+    then
+        HOSTNAME=$response
+        break
+    else
+        printf "${BOLD}${RED}Username ${RST}${BOLD}\"${response}\"${RED} is invalid!${RST}\n"
+    fi
+done
+
+if [[ -d /sys/firmware/efi ]]
+then
+    printf "${BOLD}${BLUE}Detected that system is running under UEFI${RST}\n"
+    printf "${BOLD}${YELLOW}Would you like to install for UEFI? [Y/N]: ${RST}"
+    question
+    EFI_SET=$?
+fi
+
+printf "${BOLD}${BLUE}Installation configuration:${RST}\n"
+printf "\tInstall for UEFI:\t "
+if [[ ${EFI_SET} == 0 ]]
+then
+    printf "${GREEN}true${RST}\n"
+else
+    printf "${RED}false${RST}\n"
+fi
+print "\tHostname:\t $HOSTNAME\n"
+print "\tUsername:\t $USERNAME\n"
+echo
+
+printf "${BOLD}${MAGENTA}Step 2: Select drive to install to${RST}\n"
+printf "${BOLD}${YELLOW}Are you ready to procced? [Y/N]: ${RST}"
+question_exit "Aborted installation!"
+
+DISKS=( $(lsblk -d -i -l -n -p | cut -d' ' -f1) )
+printf "${BOLD}${BLUE}Available disks:${RST}\n"
+for disk in ${DISKS[@]}
+do
+    printf "\t${disk}\n"
+done
+DISK=""
+while true
+do
+    printf "${BOLD}${YELLOW}Please select a disk from above: ${RST}"
+    read -r response
+    if [[ " ${DISKS[@]} " =~ " ${response} " ]]
+    then
+    DISK=$response
+        break
+    else
+        printf "${DISKS[*]}\n"
+        printf "${BOLD}${RED}Drive ${RST}${BOLD}\"${response}\"${RED} doesn't exist!${RST}\n"
+    fi
+done
+echo
+
+printf "${BOLD}${MAGENTA}Step 3: Installation${RST}\n"
+printf "${BOLD}${YELLOW}Are you ready to procced? [Y/N]: ${RST}"
+question_exit "Aborted installation!"
+
+printf "${BOLD}${BLUE}Running DHCPCD ...${RST}\n"
+dhcpcd
+printf "${BOLD}${BLUE}Enabling NTP ...${RST}\n"
+timedatectl set-ntp true
+printf "${BOLD}${BLUE}Formatting drive ...${RST}\n"
+if [[ $EFI_SET == 0 ]]
+then
+    fdisk ${DISK} << EOF
+g
+n
+
+
++500M
+n
+
+
+
+w
+EOF
+    mkfs.fat -F -F32 "${DISK}1"
+    mkfs.ext4 -F "${DISK}2"
+else
+    fdisk ${DISK} << EOF
+g
+n
+
+
+
+w
+EOF
+    mkfs.ext4 "${DISK}1"
+fi
+printf "${BOLD}${BLUE}Mounting filesystem ...${RST}\n"
+if [[ $EFI_SET == 0 ]]
+then
+    mount "${DISK}2" /mnt
+else
+    mount "${DISK}1" /mnt
+fi
+printf "${BOLD}${BLUE}Updating pacman databases ...${RST}\n"
+pacman -Sy
+printf "${BOLD}${BLUE}Installing archlinux-keyring ...${RST}\n"
+pacman -S --noconfirm archlinux-keyring
+printf "${BOLD}${BLUE}Installing base packages ...${RST}\n"
+pacstrap /mnt base linux linux-firmware nano reflector sudo git base-devel wget bash bash-completion openssh ntp networkmanager dhcpcd grub archlinux-keyring
+printf "${BOLD}${BLUE}Generating FSTAB ...${RST}\n"
+genfstab -U /mnt >> /mnt/etc/fstab
+printf "${BOLD}${BLUE}Setting locale ...${RST}\n"
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Athens /etc/localtime
+printf "${BOLD}${BLUE}Setting hardware clock from system time ...${RST}\n"
+arch-chroot /mnt hwclock --systohw
+printf "${BOLD}${BLUE}Generating locales ...${RST}\n"
+arch-chroot /mnt sed -i 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+arch-chroot /mnt locale-gen
+arch-chroot /mnt echo LANG=en_US.UTF-8 >> /etc/locale.conf
+printf "${BOLD}${BLUE}Generating hostname and hosts file ...${RST}\n"
+arch-chroot /mnt echo ${HOSTNAME} > /etc/hostname
+arch-chroot /mnt printf "\n127.0.0.1\t\tlocalhost\n::1\t\t\tlocalhost\n127.0.1.1\t${HOSTNAME}.localdomain\t${HOSTNAME}\n" >> /etc/hosts
+printf "${BOLD}${BLUE}Setting root password ...${RST}\n"
+arch-chroot /mnt passwd
+printf "${BOLD}${BLUE}Installing CPU microcode ...${RST}\n"
+if [[ $( cat /proc/cpuinfo | grep -m 1 "^vendor_id" | cut -d' ' -f2 ) =~ "GenuineIntel" ]]
+then
+    arch-chroot /mnt pacman -S --noconfirm intel-ucode
+else
+    arch-chroot /mnt pacman -S --noconfirm amd-ucode
+fi
+printf "${BOLD}${BLUE}Enabling systemd services ...${RST}\n"
+arch-chroot /mnt systemd enable sshd
+arch-chroot /mnt systemd enable ntpd
+arch-chroot /mnt systemd enable NetworkManager
+printf "${BOLD}${BLUE}Enabling wheel group ...${RST}\n"
+arch-chroot /mnt sed -i 's/^# %wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/' /etc/sudoers
+printf "${BOLD}${BLUE}Adding new user ...${RST}\n"
+arch-chroot /mnt "useradd -m -g users -G wheel -s /bin/bash $USERNAME"
+printf "${BOLD}${BLUE}Setting user password ...${RST}\n"
+arch-chroot /mnt "passwd $USERNAME"
+if [[ EFI_SET == 0 ]]
+then
+    printf "${BOLD}${BLUE}Installing bootloader for UEFI ...${RST}\n"
+    arch-chroot /mnt pacman -S --noconfirm efibootmgr
+    arch-chroot /mnt mkdir /efi
+    arch-chroot /mnt "mount $DISK1 /efi"
+    arch-chroot /mnt "grub-install --target=x86_64-efi --efi-directory=efi --bootloader-id=GRUB"
+else
+    printf "${BOLD}${BLUE}Installing bootloader ...${RST}\n"
+    arch-chroot /mnt "grub-install $DISK"
+fi
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+if [[ $? == 0]]
+then
+    printf "${BOLD}${GREEN}Installation script finished successfully!${RST}\n"
+else
+    printf "${BOLD}${RED}Installation script failed!${RST}\n"
+fi
